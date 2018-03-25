aef1eb954873c10e7c4965aec3c62b7762b04484
Ricky <ricky@n0rmand0.local>
2018-03-25 13:56:28 -0400

    integrate custom fields

--- wp-content/themes/grip-it-rip-it/resources/views/partials/content-home.blade.php
+++ wp-content/themes/grip-it-rip-it/resources/views/partials/content-home.blade.php
@@ -2,15 +2,12 @@
 
 <div class="showcase-container">
   <div class="showcase">
-    <div class="showcase__row">
       <div class="showcase__box"><div class="showcase__image"></div></div>
       <div class="showcase__bg"></div>
-    </div>
-    <div class="showcase__row">
+
       <div class="showcase__box"><div class="showcase__image"></div></div>
       <div class="showcase__box"><div class="showcase__image"></div></div>
       <div class="showcase__bg"></div>
-    </div>
   </div>
 </div>
 </header>
@@ -18,9 +15,9 @@
 <div class="l-section">
   <div class="l-container l-pad">
     <div class="l-full">
-      <h1>@php(the_title())</h1>
+      <h1>@php( the_field('main_headline') )</h1>
       <div class="l-2-column">
-        @php(the_content())
+        @php( the_field('main_content') )
       </div>
     </div>
   </div>
@@ -29,7 +26,8 @@
   <div class="l-container callout-container">
     <div class="callout">
       <div class="callout__box">
-        <div class="callout__box__image"></div>
+        @php($image = get_field('callout_1_image') )
+        <div class="callout__box__image" style="background-image: url({{$image['url']}})"></div>
       </div>
       <div class="callout__content">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, <em>quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</em></p>
@@ -37,7 +35,8 @@
     </div>
     <div class="callout">
       <div class="callout__box">
-        <div class="callout__box__image"></div>
+        @php($image = get_field('callout_2_image') )
+        <div class="callout__box__image" style="background-image: url({{$image['url']}})"></div>
       </div>
       <div class="callout__content">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, <em>quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</em></p>
@@ -45,7 +44,9 @@
     </div>
     <div class="callout">
       <div class="callout__box">
-        <div class="callout__box__image"></div>
+        @php($image = get_field('callout_3_image') )
+        <div class="callout__box__image" style="background-image: url({{$image['url']}})"></div>
+
       </div>
       <div class="callout__content">
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, <em>quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</em></p>
---
+++ wp-content/themes/grip-it-rip-it/resources/views/partials/content-internal.blade.php
@@ -0,0 +1,33 @@
+<header class="hero">
+  <div class="hero__title">
+    <h1>The title</h1>
+  </div>
+</header>
+
+<div class="l-section">
+
+  <div class="l-container">
+
+    <div class="flip-split">
+      <div class="flip-split__content">
+        <h2>Lorem</h2>
+        <p>Ipsum dolor</p>
+      </div>
+      <div class="flip-split__image">
+        <img src="http://via.placeholder.com/700x500" alt="">
+      </div>
+    </div>
+
+
+    <div class="flip-split">
+      <div class="flip-split__content">
+        <h2>Lorem</h2>
+        <p>Ipsum dolor</p>
+      </div>
+      <div class="flip-split__image">
+        <img src="http://via.placeholder.com/700x500" alt="">
+      </div>
+    </div>
+
+  </div>
+</div>
