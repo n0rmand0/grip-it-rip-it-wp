397125145614fc6b4d2bc5335859fa007415e2a7
Ricky <ricky@n0rmand0.local>
2018-06-09 08:09:44 -0400

    add new header-logo

--- wp-content/themes/grip-it-rip-it/resources/views/partials/footer.blade.php
+++ wp-content/themes/grip-it-rip-it/resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="site-footer l-section--secondary">
   <div id="footer-nav" class="site-footer-container">
     {{-- @php(dynamic_sidebar('sidebar-footer')) --}}
-    <a href="/course-design">Course design</a>
+    <a href="/course-design">Course Design</a>
     <a href="/events">Events</a>
 
     <img src="/wp-content/themes/grip-it-rip-it/resources/assets/images/logo.svg" alt="Grip It Rip It Logo" class="site-footer__logo">
